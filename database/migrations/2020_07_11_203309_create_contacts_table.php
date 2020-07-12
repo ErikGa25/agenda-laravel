e9e7b5098cff946354d23d69dbeed66bca2945ac
ErikGa25 <jerik67@gmail.com>
2020-07-11 21:08:19 -0500

    Se crearon las migraciones para la BD

---
+++ database/migrations/2020_07_11_203309_create_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->string('username', 30);
+            $table->string('email', 40)->unique();
+            $table->text('address');
+            $table->unsignedSmallInteger('cellphone')->unique();
+            $table->string('image', 100);
+            $table->string('job_title', 25);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
