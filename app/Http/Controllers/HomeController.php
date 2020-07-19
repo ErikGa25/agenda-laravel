d219ba878da6bb5bbace9e0fae42369d79099ab8
ErikGa25 <jerik67@gmail.com>
2020-07-19 18:28:48 -0500

    Se agrego funcion para listar usuarios y ver imagen

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $contactos =  DB::table('contacts')
+                                ->select('name', 'username', 'email', 'address', 'cellphone', 'image', 'job_title', 'sex')
+                                ->orderBy('id', 'desc')
+                                ->get();
+
+        return view('contacts.listContact', ['contactos' => $contactos]);
+    }
+
+    public function getImage($fileimage) {
+        $photo = Storage::disk('images')->get($fileimage);
+        return new response($photo, 200);
     }
 }
